32e4591f251bbded921685b4189b8e11312f0145
argens <argens@spaceshipapp.com>
2021-08-24 12:41:13 +0800

    chore(install): add lib to gitignore

--- lib/opentelemetry.constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const OPENTELEMETRY_SDK = "OPEN_TELEMETRY_SDK";
-export declare const OPENTELEMETRY_MODULE_OPTIONS = "OpenTelemetryModuleOptions";
-export declare const OPENTELEMETRY_MODULE_ID = "OpenTelemetryModuleId";
-export declare const OPENTELEMETRY_METER_PROVIDER = "OpenTelemetryMeterProvider";
--- lib/tracing/decorators/span.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Param = {
-    name?: string;
-    setStatus?: boolean;
-};
-export declare function Span(param?: Param): (target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) => void;
-export {};
